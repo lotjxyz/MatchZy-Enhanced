6ee34d10bde4644b7954a2e51666ee9487960d72
prince1456 <prince1456@users.noreply.github.com>
2026-06-17 09:03:46 -0700

    Release v1.4.21-sh7

--- src/MatchZy.cs
+++ src/MatchZy.cs
@@ -15,7 +15,7 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "1.4.21-sh6";
+        public override string ModuleVersion => "1.4.21-sh7";
 
         public override string ModuleAuthor => "sivert (https://github.com/sivert-io/)";
 
